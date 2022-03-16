ff62148d1daa6219c756a4f30442456da4d60546
Grace Stephanie Caraballo Pose <grace.caraballop@gmail.com>
2022-03-16 15:15:23 +0000

    Creada la clase calculo de area #1

---
+++ ejercicioets/area/src/main/java/es/iespuerto/area/Grace.java
@@ -0,0 +1,39 @@
+package es.iespuerto.area;
+
+/**
+ * Clase Grace que sirve parqa calcular el área de diferentes figuras
+ *
+ */
+public class Grace{
+    private String tipo;
+    private int valor1;
+    private int valor2;
+    
+    /**
+     * Constructor vacio de la clase Grace
+     */
+    public Grace() {
+    }
+
+    /**
+     * Constructor de Grace que recibe dos parámetros
+     * @param tipo  nombre de la figura(triangulo, cuadrado, rombo,...)
+     * @param valor1 valor 1 correspondiente a la figura.
+     */
+    public Grace(String tipo, int valor1) {
+        this.tipo = tipo;
+        this.valor1 = valor1;
+    }
+
+    /**
+     * Método que se encarga de calcular el área de una figura.
+     * Recibiendo por parámetro dos valores.
+     * @param valor1 valor 1 correspondiente a la figura.
+     * @param valor2 valor 2 correspondiente a la figura.
+     */
+    public void CalculoArea(int valor1, int valor2){
+        
+    }
+
+   
+}
